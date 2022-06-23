24ce6f6b498144f66a4e1e9cfcc8b02bc1b6d71b
John <john791016@gmail.com>
2022-06-23 11:22:51 +0800

    add viewset function

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,47 +1,91 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 # Create your views here.
 
+
 class HelloApiView(APIView):
     """Test API View"""
-    serializer_class=serializers.HelloSerializer
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of api feature"""
 
-        an_apiview=[
+        an_apiview = [
             'Uses HTTP methods as function (get, post, patch, put, delete)',
             'Is similar to a traditional Django View',
             'Gives you the most control over your application logic',
             'Is mapped manually to URLs',
-            ]
+        ]
 
-        return Response({'message':'Hello!','an_apiview':an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
-    def post(self,request):
-        '''Create a hello messafe with our name'''
-        serializer=self.serializer_class(data=request.data )
+    def post(self, request):
+        '''Create a hello message with our name'''
+        serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
-            name= serializer.validated_data.get('name')
-            message='Hello '+str(name)
-            return Response({'message':message})
+            name = serializer.validated_data.get('name')
+            message = 'Hello '+str(name)
+            return Response({'message': message})
         else:
             return Response(serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST)
-
+                            status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk=None):
         '''Handle updating an object'''
-        return Response({'method':'PUT'})
-    
-    def patch(self,request,pk=None):
-        """Handle a partial update of an object"""
-        return Response({'method':'PATCH'})
+        return Response({'method': 'PUT'})
 
-    def delete(self,request,pk=None):
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
         """Delete an object"""
-        return Response({'method':'DELETE'})
+        return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """return a hello message."""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message': 'Hello', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        '''Create a new hello message with our name'''
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = 'Hello '+str(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        '''Handle get an object by its ID'''
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        '''Handle an object updating'''
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        '''Handle updating partial object'''
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        '''Handle removing an object'''
+        return Response({'http_method': 'DELETE'})
