006e0af9269b6e050c5f0c54a6f589c31b86414a
John <john791016@gmail.com>
2022-06-24 09:13:22 +0800

    add permission

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,9 +6,10 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
-router.register('userProfile',views.UserProfileViewSet)
+router.register('userProfile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello_view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,11 +5,12 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
-#from profiles_api.serializers import UserProfileSerializer
+# from profiles_api.serializers import UserProfileSerializer
 # Create your views here.
 
 
@@ -97,12 +98,19 @@ class HelloViewSet(viewsets.ViewSet):
         return Response({'http_method': 'DELETE'})
 
 
-
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
-    serializer_class=serializers.UserProfileSerializer
-    queryset=models.UserProfile.objects.all()
-    Authentication_classes=(TokenAuthentication,)
-    permission_classes= (permissions.UpdateOwnProfile,)
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    Authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', 'email']
+
+
+# class UserLoginApiView(ObtainAuthToken):
+#     """Handle creating user authentication tokens"""
+#     render_classes = api_settings.DEFAULT_RENDERER_CLASSES
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
